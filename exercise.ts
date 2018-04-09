ca0b5da8cb0e0191ce458ba151769c3887047435
Sam Thomson <samt@samt.st>
2018-04-09 19:04:28 +0100

    section two exercise

---
+++ exercise.ts
@@ -0,0 +1,27 @@
+type BankAccount = {
+    money: number,
+    deposit: (value: number) => void
+}
+
+type Person = {
+    name: string,
+    bankAccount: BankAccount,
+    hobbies: string[]
+}
+
+let bankAccount: BankAccount = {
+    money: 2000,
+    deposit (value) {
+        this.money += value
+    }
+}
+
+let myself: Person = {
+    name: "Sam",
+    bankAccount: bankAccount,
+    hobbies: ["Programming", "Running", "Photography"]
+}
+
+myself.bankAccount.deposit(3000)
+
+console.log(myself)
