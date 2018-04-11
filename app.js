9a45085fdf52705574a6e7ee1c4a411bd6555ab3
Sam Thomson <samt@samt.st>
2018-04-11 19:19:02 +0100

    section 5 exercise

--- app.js
+++ app.js
@@ -1,107 +1,76 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Person = /** @class */ (function () {
-    function Person(name, username) {
-        this.username = username;
-        this.age = 29;
-        this.name = name;
-    }
-    Person.prototype.printAge = function () {
-        console.log(this.age);
-    };
-    Person.prototype.setType = function (type) {
-        this.type = type;
-        console.log(this.type);
-    };
-    return Person;
-}());
-var person = new Person('sam', 'samthomson');
-console.log(person);
-console.log(person.name, person.username);
-person.printAge();
-person.setType("[person type]");
-var Sam = /** @class */ (function (_super) {
-    __extends(Sam, _super);
-    // name = "Sam"
-    function Sam(username) {
-        var _this = _super.call(this, "Sam", username) || this;
-        _this.age = 29.5;
-        return _this;
-    }
-    return Sam;
-}(Person));
-var sam = new Sam('samthomson');
-console.log(sam);
-// Getters & Setters
-var Plant = /** @class */ (function () {
-    function Plant() {
-        this._species = "Default";
-    }
-    Object.defineProperty(Plant.prototype, "species", {
-        get: function () {
-            return this._species;
-        },
-        set: function (value) {
-            if (value.length > 3) {
-                this._species = value;
-            }
-            else {
-                this._species = "Default";
-            }
-        },
-        enumerable: true,
-        configurable: true
-    });
-    return Plant;
-}());
-var plant = new Plant();
-console.log(plant.species);
-plant.species = "AB";
-console.log(plant.species);
-plant.species = "Green plant";
-console.log(plant.species);
-// Static Properties & Methods
-var Helpers = /** @class */ (function () {
-    function Helpers() {
-    }
-    Helpers.calcCircumference = function (diameter) {
-        return this.PI * diameter;
-    };
-    Helpers.PI = 3.14;
-    return Helpers;
-}());
-console.log(2 * Helpers.PI);
-console.log(Helpers.calcCircumference(8));
-// Abstract classes
-var Project = /** @class */ (function () {
-    function Project() {
-        this.projectName = "Default";
-        this.budget = 1000;
-    }
-    Project.prototype.calcBudget = function () {
-        return this.budget * 2;
-    };
-    return Project;
-}());
-var ITProject = /** @class */ (function (_super) {
-    __extends(ITProject, _super);
-    function ITProject() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    ITProject.prototype.changeName = function (name) {
-        this.projectName = name;
-    };
-    return ITProject;
-}(Project));
-var newProject = new ITProject();
-console.log(newProject);
-newProject.changeName("Super IT Project");
-console.log(newProject);
+// class Person {
+//     name: string
+//     private type: string
+//     protected age: number = 29
+//     constructor(name: string, public username: string) {
+//         this.name = name
+//     }
+//     printAge() {
+//         console.log(this.age)
+//     }
+//     setType(type: string) {
+//         this.type = type
+//         console.log(this.type)
+//     }
+// }
+// const person = new Person('sam', 'samthomson')
+// console.log(person)
+// console.log(person.name, person.username)
+// person.printAge()
+// person.setType("[person type]")
+// class Sam extends Person {
+//     // name = "Sam"
+//     constructor(username: string) { 
+//         super("Sam", username)
+//         this.age = 29.5
+//     }
+// }
+// const sam = new Sam('samthomson')
+// console.log(sam)
+// // Getters & Setters
+// class Plant {
+//     private _species: string = "Default"
+//     get species() {
+//         return this._species
+//     }
+//     set species(value: string) {
+//         if (value.length > 3) {
+//             this._species = value
+//         } else {
+//             this._species = "Default"
+//         }
+//     }
+// }
+// let plant = new Plant()
+// console.log(plant.species)
+// plant.species = "AB"
+// console.log(plant.species)
+// plant.species = "Green plant"
+// console.log(plant.species)
+// // Static Properties & Methods
+// class Helpers {
+//     static PI: number = 3.14
+//     static calcCircumference(diameter: number): number {
+//         return this.PI * diameter
+//     }
+// }
+// console.log(2 * Helpers.PI)
+// console.log(Helpers.calcCircumference(8))
+// // Abstract classes
+// abstract class Project {
+//     projectName: string = "Default"
+//     budget: number = 1000
+//     abstract changeName(name: string): void
+//     calcBudget(){
+//         return this.budget * 2
+//     }
+// }
+// class ITProject extends Project {
+//     changeName(name: string): void {
+//         this.projectName = name
+//     }
+// }
+// let newProject = new ITProject()
+// console.log(newProject)
+// newProject.changeName("Super IT Project")
+// console.log(newProject)
---
+++ section_five_exercise.ts
@@ -0,0 +1,71 @@
+// Exercise 1 - How was your TypeScript Class?
+class Car {
+
+    name: string
+    acceleration: number = 0
+
+    constructor (name: string) {
+        this.name = name
+    }
+
+    honk(): void {
+        console.log("Toooooooooot!");
+    };
+ 
+    accelerate(speed: number): void {
+        this.acceleration += speed;
+    }
+}
+let car = new Car("BMW")
+car.honk()
+console.log(car.acceleration);
+car.accelerate(10);
+console.log(car.acceleration);
+ 
+// Exercise 2 - Two objects, based on each other ...
+class BaseObject {
+
+    width: number = 0
+    length: number = 0
+
+    constructor() {
+    }
+}
+
+class Rectangle extends BaseObject {
+    constructor(width: number, length: number) {
+        super()
+        this.width = width
+        this.length = length
+    }
+
+    calcSize():number {
+        return this.width * this.length
+    }
+}
+
+let rectangle = new Rectangle(12, 4)
+console.log(rectangle.calcSize());
+ 
+// Exercise 3 - Make sure to compile to ES5 (set the target in tsconfig.json)
+class Person {
+    private _firstName: string = ''
+
+    get firstName() {
+        return this._firstName
+    }
+
+    set firstName(value: string) {
+        if (value.length > 2) {
+            this._firstName = value
+        }else{
+            this._firstName = ''
+        }
+    }
+}
+let person = new Person
+console.log(person.firstName);
+person.firstName = "Sa";
+console.log('name: ', person.firstName);
+person.firstName = "Sam";
+console.log('name: ', person.firstName);
