041211999525cf1802e5c9bbef91df9f55af06da
Sam Thomson <samt@samt.st>
2018-04-17 18:43:01 +0100

    section eight

---
+++ section_eight_exercise.ts
@@ -0,0 +1,41 @@
+/*
+
+setItem(key: string, item: T) // should create a new key-value pair
+ 
+getItem(key: string) // should retrieve the value of the provided key
+clear() // should remove all key-value pairs
+printMap() // should output key-value pairs
+
+*/
+
+class MyMap<T> {
+    private map: {[key: string]: T} = {}
+
+    setItem(key: string, item: T): void {
+        this.map[key] = item
+    }
+
+    getItem(key: string): T {
+        return this.map[key]
+    }
+
+    clear(): void {
+        this.map = {}
+    }
+
+    printMap(): void {
+        for (let key in this.map) {
+            console.log(key, this.map[key])
+        }
+    }
+}
+
+const numberMap = new MyMap<number>();
+numberMap.setItem('apples', 5);
+numberMap.setItem('bananas', 10);
+numberMap.printMap();
+ 
+const stringMap = new MyMap<string>();
+stringMap.setItem('name', "Max");
+stringMap.setItem('age', "27");
+stringMap.printMap();
